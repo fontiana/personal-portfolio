a6a275c01a150f9ef5976689c408668a300d01fd
Victor Oliveira <victoroliveira@Victors-MacBook-Pro.local>
2018-10-30 23:05:29 -0300

    Reordering solution

---
+++ Solution/Personal.Portfolio/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Personal.Portfolio.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
