669424e3527977c6977e029f4cfa28ad663935af
fontiana <fontiana@gmail.com>
2018-11-05 23:47:35 -0200

    Adicionando layouts

--- Solution/Personal.Portfolio/Controllers/HomeController.cs
+++ Solution/Personal.Portfolio/Controllers/HomeController.cs
@@ -21,11 +21,6 @@ namespace Personal.Portfolio.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
