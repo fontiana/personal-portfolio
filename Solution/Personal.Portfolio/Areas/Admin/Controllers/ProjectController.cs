260d48f5c6263deefc8b1493dfb7cabd7c1006ea
fontiana <fontiana@gmail.com>
2019-01-20 16:28:45 -0200

    Addding models

--- Solution/Personal.Portfolio/Areas/Admin/Controllers/ProjectController.cs
+++ Solution/Personal.Portfolio/Areas/Admin/Controllers/ProjectController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Personal.Portfolio.Areas.Admin.Models;
 
 namespace Personal.Portfolio.Areas.Admin.Controllers
 {
@@ -10,6 +11,36 @@ namespace Personal.Portfolio.Areas.Admin.Controllers
     public class ProjectController : Controller
     {        
         public IActionResult Index()
+        {
+            return View();
+        }
+        
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult Add(ProjectViewModel model)
+        {
+            return View();
+        }
+        
+        [HttpGet]
+        public IActionResult Edit()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult Edit(ProjectViewModel model)
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult Delete(int id)
         {
             return View();
         }
---
+++ Solution/Personal.Portfolio/Areas/Admin/Models/ProjectViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Personal.Portfolio.Areas.Admin.Models
+{
+    public class ProjectViewModel
+    {
+        public ProjectViewModel()
+        {
+        
+        }
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
