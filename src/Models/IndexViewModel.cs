5d36671eb3caa82f56d1bf6773013b8b7dc5f1ad
victor oliveira <victor.oliveira@avanade.com>
2021-03-24 01:27:44 -0300

    fix: fixing declaration

--- src/Models/IndexViewModel.cs
+++ src/Models/IndexViewModel.cs
@@ -4,7 +4,7 @@ namespace PersonalPortfolio.Models
 {
     public class IndexViewModel
     {
-        public List<PostViewModel> Posts{ get; set; }
-        public List<ProjectViewModel> Projects { get; set; }
+        public List<PostViewModel> Posts { get; set; } = new List<PostViewModel>();
+        public List<ProjectViewModel> Projects { get; set; } = new List<ProjectViewModel>();
     }
 }
