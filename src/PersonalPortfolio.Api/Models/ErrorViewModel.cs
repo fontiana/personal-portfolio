80e4f5882f6e2f1fdd1fe2c9836e89a268dc35e2
victor oliveira <victor.oliveira@avanade.com>
2021-04-27 18:18:33 -0300

    test: removing unused code

--- src/PersonalPortfolio.Api/Models/ErrorViewModel.cs
+++ src/PersonalPortfolio.Api/Models/ErrorViewModel.cs
@@ -5,7 +5,5 @@ namespace PersonalPortfolio.Models
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
